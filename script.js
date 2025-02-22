5fad0f16f200c77c428bfbf7d6b4519712d12ccb
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-22 23:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function binarySearch(nums, target) {
-// Your code here
-// Return index if target is present
-// else return -1
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        if (nums[mid] === target) {
+            return mid; // Target found, return index
+        } else if (nums[mid] < target) {
+            left = mid + 1; // Search right half
+        } else {
+            right = mid - 1; // Search left half
+        }
+    }
+    
+    return -1; // Target not found
 }
 
 module.exports = binarySearch;
